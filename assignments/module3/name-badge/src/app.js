d1cea01260fd9b54c278f48d5fa25cab202de134
David Wangen <david.wangen@gmail.com>
2022-05-10 23:42:18 -0500

    started name-badge

---
+++ assignments/module3/name-badge/src/app.js
@@ -0,0 +1,26 @@
+import React, {Component} from "react"
+
+class App extends Component {
+    constructor(){
+        super()
+        this.state ={}
+    }
+
+    render (){
+        return (
+            <main>
+                <form>
+                    <input></input>
+                    <input></input>
+                    <input></input>
+                    <input></input>
+                    <input></input>
+                    <input></input>
+                    <input></input>
+                </form>
+            </main>
+        )
+    }
+}
+
+export default App
